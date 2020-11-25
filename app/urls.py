97895822dc5a6c432c8e30348f12fd0cca382c6b
j17012 <j17012@sangi.jp>
2020-11-25 10:55:32 +0900

    update-201125

--- app/urls.py
+++ app/urls.py
@@ -6,12 +6,6 @@ app_name = 'app'
 urlpatterns = [
     path('top/', views.index, name='index'),
     path('player/', views.player, name='player'),
-    path('cut/', views.Upload.as_view(), name='cut'),
-<<<<<<< HEAD
     path('upload/', views.SingleUploadWithModelView.as_view(), name='upload'),
-    path('', views.FileListView.as_view(), name='file_list'),
-=======
-    path('upload/', views.UploadView.as_view(), name='upload'),
-    path("ajax/", views.call_save_frames,name="call_save_frames")
->>>>>>> 14383aa0fe9ec94706f89a7ad9fa40ef2c374213
+    path('file_list/', views.FileListView.as_view(), name='file_list'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,9 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.http import HttpResponse
 from .forms import  SingleUploadModelForm
-from .cuts import UploadForm
 from .models import UploadFile
-from . import cuts
 
 def index(request):
     return render(request, 'app/index.html')
@@ -13,29 +11,11 @@ def index(request):
 def player(request):
     return render(request, 'app/player.html')
 
-def cut(request):
-    return render(request, 'app/cut.html')
-
 #動画分割
-class Upload(generic.FormView):
-    form_class = UploadForm
-    template_name = 'app/cut.html'
-
-    def form_valid(self, form):
-        download_url = form.save()
-        context = {
-            'download_url': download_url,
-            'form': form,
-        }
-        return self.render_to_response(context)
+class UploadListView(generic.ListView):
+    #アップロードされたファイルの一覧ページ
+    model = UploadFile
     
-    def call_save_frames(request):
-        if request.method == 'POST':
-            #cut.pyのsave_framesを呼び出す
-            #ajaxで送信したデータのうちimput_dataを指定して取得する
-            cuts.save_frames(request.POST.get("imput_data"))
-            return HttpResponse()
-
 #動画アップロード
 class SingleUploadWithModelView(generic.CreateView):
     #ファイルモデルのアップロードビュー
