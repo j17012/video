7cb3a7903b4c9fc7c33f56112c590fa1a1df5976
j17012 <j17012@sangi.jp>
2020-11-26 13:55:21 +0900

    update

--- app/cuts.py
+++ app/cuts.py
@@ -3,15 +3,6 @@ import os
 from os.path import splitext, dirname, basename, join
 from os import makedirs
 from django import forms
-from django.core.files.storage import default_storage
-
-class UploadForm(forms.Form):
-    file = forms.FileField(label='動画ファイル')
-
-    def save(self):
-        upload_file = self.cleaned_data['file']
-        file_name = default_storage.save(upload_file.name, upload_file)
-        return default_storage.url(file_name)
 
     def save_frames(video_path: str, frame_dir: str, 
                     name="image", ext="jpg"):
