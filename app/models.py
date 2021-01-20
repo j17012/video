5a50e49f59d76949dc65b1c35ed2391fc1339506
krtbd <46546498+krtbd@users.noreply.github.com>
2021-01-20 12:41:45 +0900

    Update models.py

--- app/models.py
+++ app/models.py
@@ -20,11 +20,12 @@ class UploadImage(models.Model):
     
 class Label_Info(models.Model):
     """フレーム毎のラベル情報を表すモデル"""
-    sec = models.IntegerField(null=True,blank=True)
-    man = models.IntegerField(null=True,blank=True)
-    pc_char = models.IntegerField(null=True,blank=True)
-    white_board = models.IntegerField(null=True,blank=True)
-    char_red = models.IntegerField(null=True,blank=True)
-    char_yellow = models.IntegerField(null=True,blank=True)
-    human_char = models.IntegerField(null=True,blank=True)
+    ###mov_name = models.CharField('動画名', blank=True, max_length=100)###
+    sec = models.IntegerField('時間', blank=True, null=True)
+    man = models.IntegerField('男性', blank=True, null=True)
+    pc_char = models.IntegerField('PC', blank=True, null=True)
+    white_board = models.IntegerField('白板', blank=True, null=True)
+    char_red = models.IntegerField('赤文字', blank=True, null=True)
+    char_yellow = models.IntegerField('黄文字', blank=True, null=True)
+    human_char = models.IntegerField('人', blank=True, null=True)
 
